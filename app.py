4b146e1a03628e318d3e41eaefcb7f3748dc8abb
pranavs28 <pranav.senthilvel@gmail.com>
2022-04-19 13:11:16 -0400

    Added requests usecase for SPARQL queries (app.py:5-11)

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 from flask import Flask, render_template
 
+import requests
+
+# url = 'https://query.wikidata.org/sparql'
+# query = '''
+# SELECT ?item ?instance_of ?instance_ofLabel WHERE {
+#   ?item wdt:P131 wd:Q464266.
+#   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+#   OPTIONAL { ?item wdt:P18 ?instance_of. }
+# }
+# '''
+
+# r = requests.get(url, params = {'format': 'json', 'query': query})
+# data = r.json()
+# print(data)
+
+#ABOVE INFO CAN BE USED FOR QUERY ENTRIES
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
