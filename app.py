3d6c523edb31bae71876d1d043e3921e8d96d175
brjenn <58012580+brjenn@users.noreply.github.com>
2022-05-03 19:34:01 -0400

    Pages and querying done for each object

--- app.py
+++ app.py
@@ -36,14 +36,14 @@ WHERE
 }
 '''
 
-r = requests.get(url, params = {'format': 'json', 'query': query})
-data = r.json()
-# https://stackoverflow.com/questions/55961615/how-to-integrate-wikidata-query-in-python
+# r = requests.get(url, params = {'format': 'json', 'query': query})
+# data = r.json()
+# # https://stackoverflow.com/questions/55961615/how-to-integrate-wikidata-query-in-python
 
-#ABOVE INFO CAN BE USED FOR QUERY ENTRIES
+# #ABOVE INFO CAN BE USED FOR QUERY ENTRIES
 
-r = requests.get(url, params = {'format': 'json', 'query': q2})
-d2 = r.json()
+# r = requests.get(url, params = {'format': 'json', 'query': q2})
+# d2 = r.json()
 
 
 
@@ -60,8 +60,87 @@ def scratch():
 @app.route("/qr_code")
 def qr_code():
     return render_template('qr_code.html')
-@app.route("/object")
-def object():
+@app.route("/latin_inscription")
+def latin_inscription():
+    queries = load_queries("SPARQL-queries/latin-inscription-translations.rq", "SPARQL-queries/latin-inscription-depicts.rq")
+    data = query_wd(queries[0], queries[1])
+
+    info = parse_query_data(data[0], data[1])
+
+    return render_template('latin_object.html', image=info[0], translations=info[1], depictions=info[2])
+    # image = data["results"]["bindings"][0]["image"]["value"]
+    # all_transl = data["results"]["bindings"]
+    # translations = []
+
+    # for i in range(len(all_transl)):
+    #     temp = []
+    #     temp.append(all_transl[i]["inscription"]["value"])
+    #     temp.append(all_transl[i]["translation"]["value"])
+    #     temp.append(split_pos(all_transl[i]["position"]["value"]))
+
+        
+    #     translations.append(temp)
+    
+    # # print(translations)
+    # # print(d2)
+
+    # all_depicts = d2["results"]["bindings"]
+    # depictions = []
+
+    # for i in range(len(all_depicts)):
+    #     temp = []
+    #     temp.append(all_depicts[i]["depiction"]["value"])
+    #     temp.append(split_pos(all_depicts[i]["position"]["value"]))
+    #     depictions.append(temp)
+
+    # return render_template('latin_object.html', image=image, translations=translations, depictions=depictions)
+
+@app.route("/arsu_relief")
+def arsu_relief():
+    queries = load_queries("SPARQL-queries/arsu-relief-translations.rq", "SPARQL-queries/arsu-relief-depicts.rq")
+    data = query_wd(queries[0], queries[1])
+
+    info = parse_query_data(data[0], data[1])
+
+    return render_template('arsu_relief.html', image=info[0], translations=info[1], depictions=info[2])
+
+@app.route("/gad_relief")
+def gad_relief():
+    queries = load_queries("SPARQL-queries/gad-relief-translations.rq", "SPARQL-queries/gad-relief-depicts.rq")
+    data = query_wd(queries[0], queries[1])
+
+    info = parse_query_data(data[0], data[1])
+
+    return render_template('gad_relief.html', image=info[0], translations=info[1], depictions=info[2])
+
+@app.route("/julius_terentius")
+def julius_terentius():
+    queries = load_queries("SPARQL-queries/julius-terentius-translations.rq", "SPARQL-queries/julius-terentius-depicts.rq")
+    data = query_wd(queries[0], queries[1])
+
+    info = parse_query_data(data[0], data[1])
+
+    return render_template('julius_terentius.html', image=info[0], translations=info[1], depictions=info[2])
+
+@app.route("/mithras_relief")
+def mithras_relief():
+    queries = load_queries("SPARQL-queries/mithras-relief-translations.rq", "SPARQL-queries/mithras-relief-depicts.rq")
+    data = query_wd(queries[0], queries[1])
+
+    info = parse_query_data(data[0], data[1])
+
+    return render_template('mithras_relief.html', image=info[0], translations=info[1], depictions=info[2])
+
+@app.route("/votive_relief")
+def votive_relief():
+    queries = load_queries("SPARQL-queries/votive-relief-translations.rq", "SPARQL-queries/votive-relief-depicts.rq")
+    data = query_wd(queries[0], queries[1])
+
+    info = parse_query_data(data[0], data[1])
+
+    return render_template('votive_relief.html', image=info[0], translations=info[1], depictions=info[2])
+
+def parse_query_data(data, d2):
     image = data["results"]["bindings"][0]["image"]["value"]
     all_transl = data["results"]["bindings"]
     translations = []
@@ -87,7 +166,25 @@ def object():
         temp.append(split_pos(all_depicts[i]["position"]["value"]))
         depictions.append(temp)
 
-    return render_template('latin_object.html', image=image, translations=translations, depictions=depictions)
+    return [image, translations, depictions]
+
+def load_queries(fname_1, fname_2):
+    with open(fname_1, 'r') as file:
+        query_1 = file.read()
+
+    with open(fname_2, 'r') as file:
+        query_2 = file.read()
+    # print(query_1)
+    return [query_1, query_2]
+
+def query_wd(query, q2):
+    r = requests.get(url, params = {'format': 'json', 'query': query})
+    data = r.json()
+
+    r = requests.get(url, params = {'format': 'json', 'query': q2})
+    d2 = r.json()
+
+    return [data, d2]
 
 def split_pos(pos_string):
     pct = []
